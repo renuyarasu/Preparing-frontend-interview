72ccc25acb589116a56b0eb5879475a845d3a57d
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-20 21:37:24 +0530

    Variable Declarations: Problem 01

--- script.js
+++ script.js
@@ -3,13 +3,9 @@ console.clear();
 
 // 02. Variable Declarations - const vs let vs const
 
-// Hoisting: Hoisting means moving variable declarations to the top of their scope. 
-// Ex: 01 -  Variable Declarations
-
-function hoist() {
-    console.log(name);
+function exercise() {
     var name = 'VedaGna';
+    var name = 'Yarasu';
+    console.log(name);
 }
-hoist();
-
-// ** Only variable declarations are hoisted to the top and not variable initializations.
+exercise()
