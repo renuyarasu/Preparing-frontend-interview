af495935e1cd8947f084da63e3502d3c0931a047
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-20 21:17:58 +0530

    Variable Declarations

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // Learn
 console.clear();
+
+// 02. Variable Declarations - var vs let vs const
+
+// Variable Declarations 
+
+function addNumbers() {
+    var a = 10;
+    var b = 20;
+    var flag = true;
+    if (flag) {
+        var sum = a + b;
+        console.log('Inside: ', sum);
+    }
+    console.log('Outside: ', sum);
+}
+addNumbers();
