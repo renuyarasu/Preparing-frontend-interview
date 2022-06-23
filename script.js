45131c3a3c62699b6703bd168274daf056ea2384
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-23 16:58:16 +0530

    Scope: Ex 06

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ console.clear();
 // Global/ Function Scope
 const a = 10;
 function example() {
-    console.log(a);
+    const b = 20;
+    function inner() {
+        const c = 30;
+        console.log(a, b, c);
+    }
+    inner()
 }
-example(); // 10
+example(); // 10 20 30
