1b03c2f8d1d030d126c9b91205a9329773b51fc2
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-23 18:12:49 +0530

    Closures Exercise

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 // Learn
 console.clear();
-
 // Closures
 
-function outer() {
-    let counter = 0;
-    function inner() {
-        counter++;
-        console.log(counter);
-    }
-    return inner;
+function square(num) {
+    return num * num;
 }
-let fn = outer();
-fn(); // 1
-fn(); // 1
-fn(); // 1
-fn(); // 1
-fn(); // 1
-fn(); // 1
-fn(); // 1
+
+function memoizedSquare() {
+    let cache = {};
+    return function optimizedSquare(num) {
+        if (num in cache) {
+            console.log('Returning from cache')
+            return cache[num]
+        } else {
+            console.log('Computing Square')
+            const result = square(num)
+            cache[num] = result
+            return result
+        }
+
+    }
+}
+
+const memoSquare = memoizedSquare();
+console.log(memoSquare(2)); // Computing Square
+console.log(memoSquare(2)); // Returning from cache
+
+console.log(memoSquare(5)); // Computing Square
+console.log(memoSquare(5)); // Returning from cache
