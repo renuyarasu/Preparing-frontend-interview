d4ab9c413f3338ac2005149432c71a53abf4dffb
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-23 16:50:18 +0530

    Scope: Ex 01

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 // Learn
 console.clear();
 
-// 02. Variable Declarations - const vs let vs const
+// Scope
 
-// Problem 04
-
-function profile() {
-    fName: 'VedaGna';
-    lName: 'Yarasu';
-}
-profile.fName = 'Gnapika';
-console.log(profile.fName);
-
-/* 
-01. Even through an object is declared with const, yiou can still mutate its properties
-02. You can't ressign a value like an empty object to the profile variable 
-03. profile= {} */
+const a = 10;
+console.log(a); // 10
